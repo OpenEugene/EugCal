cd9db92bc77c942293586a81fa6d39f58e6bc8bc
Mark Davis <github@nohorse.com>
2020-03-07 22:30:23 -0800

    so close but sql connection error

--- EugCal/Startup.cs
+++ EugCal/Startup.cs
@@ -57,7 +57,7 @@ namespace EugCal
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
